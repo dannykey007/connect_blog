f4deb916db56cad6a470b5d0c73e25ffd49b9c36
Dannykey007 <urumdaniel179@gmail.com>
2026-03-27 05:54:38 +0100

    Auto-create admin user

--- dj_connect/wsgi.py
+++ dj_connect/wsgi.py
@@ -7,6 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
+from django.contrib.auth import get_user_model
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +15,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dj_connect.settings')
 
 application = get_wsgi_application()
+
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser(
+        'admin', 'urumdaniel179@mail.com', 'tcennoc_jd')
