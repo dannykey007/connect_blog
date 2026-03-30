e75d8082a5333bb2054e8b414e2c109f5ce535b1
Dannykey007 <urumdaniel179@gmail.com>
2026-03-30 03:59:14 +0100

    Add WhiteNoise middleware for static files

--- dj_connect/settings.py
+++ dj_connect/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -54,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', # Add this line!
+    # Add this line!
     # ... other middlewares ...
 ]
 
